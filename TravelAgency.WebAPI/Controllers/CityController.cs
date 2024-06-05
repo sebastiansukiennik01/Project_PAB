69643bf8ddd916d832517898a4421526bed0229c
Wojtek Seweryn <wojciech.seweryn29@gmail.com>
2024-06-06 00:48:27 +0200

    full controller for city

--- TravelAgency.WebAPI/Controllers/CityController.cs
+++ TravelAgency.WebAPI/Controllers/CityController.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using TravelAgency.Infrastructure;
+using TravelAgency.Domain.Exceptions;
+using TravelAgency.Application.Services;
 using TravelAgency.SharedKernel.Dto;
 
 namespace TravelAgency.WebAPI.Controllers
@@ -9,32 +9,74 @@ namespace TravelAgency.WebAPI.Controllers
     [Route("[controller]")]
     public class CityController : Controller
     {
-        private readonly DatabaseContext _context;
+        private readonly ICityService _cityService;
+        private readonly ILogger<CityController> _logger;
 
-        public CityController(DatabaseContext _context)
+        public CityController(ICityService cityService, ILogger<CityController> logger)
         {
-            this._context = _context;
+            this._cityService = cityService;
+            _logger = logger;
         }
 
 
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public ActionResult<IEnumerable<CityDto>> Get()
         {
-            
-            var cities = _context.City.ToList();
-
-            List<CityDto> result = new List<CityDto>();
-
-            foreach (var city in cities)
-            {
-                result.Add(new CityDto()
-                {
-                    CityId = city.CityId,
-                    Name = city.Name,
-                });
-            }
-
+            var result = _cityService.GetAll();
+            _logger.LogDebug("Pulled a list of all cities");
             return Ok(result);
         }
+
+        [HttpGet("{id}", Name = "GetCity")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<CityDto> Get(int id)
+        {
+            var result = _cityService.GetById(id);
+            _logger.LogDebug($"Pulled a city with id = {id}");
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult Create([FromBody] CreateCityDto dto)
+        {
+            var id = _cityService.Create(dto);
+            _logger.LogDebug($"Created new city with id = {id}");
+            var actionName = nameof(Get);
+            var routeValues = new { id };
+            return CreatedAtAction(actionName, routeValues, null);
+        }
+
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult Update(int id, [FromBody] UpdateCityDto dto)
+        {
+            if (id != dto.CityId)
+            {
+                throw new BadRequestException("Id param is not valid");
+            }
+
+            _cityService.Update(dto);
+            _logger.LogDebug($"Updated city with id = {id}");
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult Delete(int id)
+        {
+            _cityService.Delete(id);
+            _logger.LogDebug($"Deleted city with id = {id}");
+            return NoContent();
+        }
     }
+
 }
--- TravelAgency.WebAPI/Program.cs
+++ TravelAgency.WebAPI/Program.cs
@@ -1,41 +1,93 @@
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using Microsoft.EntityFrameworkCore;
+using NLog;
+using NLog.Web;
+using TravelAgency.Application.Mappings;
+using TravelAgency.Application.Services;
+using TravelAgency.Application.Validators;
+using TravelAgency.Domain.Contracts;
 using TravelAgency.Infrastructure;
+using TravelAgency.Infrastructure.Repositories;
+using TravelAgency.SharedKernel.Dto;
+using TravelAgency.WebAPI.Middleware;
 
-var builder = WebApplication.CreateBuilder(args);
+// Early init of NLog to allow startup and exception logging, before host is built
+var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+logger.Debug("init main");
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var sqliteConnectionString = "Data Source=TravelAgency.WebAPI.database.db";
-builder.Services.AddDbContext<DatabaseContext>(options =>
-    options.UseSqlite(sqliteConnectionString));
-
-builder.Services.AddScoped<DataSeeder>();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var builder = WebApplication.CreateBuilder(args);
+
+    // NLog: Setup NLog for Dependency injection
+    builder.Logging.ClearProviders();
+    builder.Host.UseNLog();
+
+    // Add services to the container.
+
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    builder.Services.AddAutoMapper(typeof(TravelAgencyMappingProfile));
+
+    builder.Services.AddFluentValidationAutoValidation();
+
+    var sqliteConnectionString = "Data Source=TravelAgency.WebAPI.database.db";
+    builder.Services.AddDbContext<DatabaseContext>(options =>
+        options.UseSqlite(sqliteConnectionString));
+
+    builder.Services.AddScoped<IValidator<CreateCityDto>, RegisterCreateCityDtoValidator>();
+    builder.Services.AddScoped<IValidator<UpdateCityDto>, RegisterUpdateCityDtoValidator>();
+
+    builder.Services.AddScoped<DataSeeder>();
+
+    builder.Services.AddScoped<ITravelAgencyUnitOfWork, TravelAgencyUnitOfWork>();
+
+    builder.Services.AddScoped<ICityService, CityService>();
+    builder.Services.AddScoped<IOfferRepository, OfferRepository>();
+    builder.Services.AddScoped<ICityRepository, CityRepository>();
+
+    builder.Services.AddScoped<ExceptionMiddleware>();
+
+    var app = builder.Build();
+
+    app.UseStaticFiles();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseMiddleware<ExceptionMiddleware>();
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    // seeding data
+    using (var scope = app.Services.CreateScope())
+    {
+        var dataSeeder = scope.ServiceProvider.GetRequiredService<DataSeeder>();
+        dataSeeder.Seed();
+    }
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-// seeding data
-using (var scope = app.Services.CreateScope())
+catch (Exception exception)
 {
-    var dataSeeder = scope.ServiceProvider.GetRequiredService<DataSeeder>();
-    dataSeeder.Seed();
+    // NLog: catch setup errors
+    logger.Error(exception, "Stopped program because of exception");
+    throw;
 }
-
-app.Run();
+finally
+{
+    // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+    NLog.LogManager.Shutdown();
+}
